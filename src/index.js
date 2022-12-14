5696fba431d87c381c1c96f4667324dc2c6607e9
Cesar Napoleon Mejia <cmejia@gmail.com>
2022-12-14 09:29:31 +0700

    interim

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import '../assets/css/style.css'
-import jsImage from '../assets/img/javascript.svg'
 
 const app = document.getElementById('app')
-app.innerHTML = `<div style="display: flex; justify-content: center; align-items: bottom"><img style="margin-right: 20px;" src=${jsImage} alt="JS img"/><h1>JavaScript DOM</h1></div>`
+app.innerHTML = `<div style="display: flex; justify-content: center; align-items: bottom"><h1>JavaScript DOM</h1></div>`
 
 addEventListener('DOMContentLoaded', () => {
   console.log('binb bin')
